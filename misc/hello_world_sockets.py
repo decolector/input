60794f9bffb969ceea001fc89918c1d06410d618
decolector <cmart@decolector.net>
2014-04-13 13:43:19 -0300

    sockets test updat

--- misc/hello_world_sockets.py
+++ misc/hello_world_sockets.py
@@ -1,6 +1,6 @@
 from socket import *
 s = socket(AF_INET, SOCK_STREAM)
-addr = ("192.168.1.105",9520)
+addr = ("192.168.1.101",9520)
 s.connect(addr)
 string = '015A3030024141061B30621C311A310A49310A4F3157656C636F6D6504'
 hex_str = string.decode("hex")
