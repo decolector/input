b1c23e050d73661ed9156535be3d75fcb26d1c85
decolector <cmart@decolector.net>
2014-04-13 10:18:49 -0300

    some error handling

---
+++ display/display-couch.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+from datetime import datetime
+import re
+from threading import Thread, Timer
+from socket import *
+import logging
+logging.basicConfig()
+import couchdbkit
+from apscheduler.scheduler import Scheduler
+import xml.etree.ElementTree as ET
+
+from JetFileII import Font
+from JetFileII import Animate
+from JetFileII import Format
+from JetFileII import Date
+from JetFileII import Message
+
+class LedDisplay():
+	"""Class to manage a led sign"""
+
+	def __init__(self, db_host, db_name, display_addr, display_port, endpoint):
+
+		#super(LedDisplay, self).__init__()
+		#Thread.__init__(self)
+		self.displayMsg = Message.DisplayControlWithoutChecksum
+		self.addr = display_addr
+		self.port = display_port
+		#configure display
+		#currently static values for my display
+		self.groupAddr = 1
+		self.unitAddr = 1
+		self.widthPixels = 120
+		self.heightPixels = 7
+		self.msg = None
+		self.dbserver = couchdbkit.Server(db_host)
+		self.db = self.dbserver.get_or_create_db(db_name)
+		self.endpoint = endpoint
+
+	def sendMessage(self, text):
+	 	self.msg = self.displayMsg.Create(1,text=text)
+		#create connection with display
+		s = socket(AF_INET, SOCK_STREAM)
+
+ 		s.connect((self.addr,self.port))
+		#send message and close connection
+		print "Sending messsage to display"
+		s.send(self.msg)
+		s.close()
+
+
+	def query(self):
+			
+		text = ''
+		print 'Hitting database...'
+		for doc in self.db.all_docs( limit=5, descending=False, include_docs=True):
+			author = doc['doc']['author']
+			body = doc['doc']['body']
+			print 'message: ', body, ' by ', author
+			linea = '{red}{7x6}{slowest}{moveleftin}{moveleftout}{left}{left}{pause}' + body + ' > (' + str(author) + ')                         '
+			text += linea
+
+		
+		print "Message built"
+		self.sendMessage(text)
+
+
+
+def main():
+
+	config_file = "config.xml"
+	xml = ET.parse(config_file)
+	HOST_NAME = xml.find('host_name').text
+	DB_NAME = xml.find('db_name').text
+	DISPLAY_ADDR = xml.find("display_addr").text
+	DISPLAY_PORT = int(xml.find('display_port').text)
+	ENDPOINT = xml.find("endpoint").text
+	SECONDS = int(xml.find("seconds").text)
+	USERNAME = xml.find("username").text
+	PASSWORD = xml.find("password").text
+
+	HOST = 'http://'+ USERNAME+ ':'+ PASSWORD + '@'+ HOST_NAME
+
+	print "starting ..."
+	
+	#print HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT
+	#readData(HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT)
+	display = LedDisplay(HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT, ENDPOINT)
+	# Start the scheduler
+	sched = Scheduler()
+
+	sched.add_interval_job(display.query, seconds=SECONDS )
+	sched.start()
+
+	try: 
+		while True:
+			time.sleep(0.1)
+
+	except KeyboardInterrupt:
+		print "terminating"
+		sched.shutdown()
+
+
+if __name__ == "__main__":
+	main()
+
+	#configure http client
+	#base_url = "https://decolector.iriscouch.com/messages/"
+	#url =  base_url + '_all_docs'
+	#text = '{red}{7x6}{randomin}{randomout}Hello{nl}World{nl}JetFile{nl}'
+
+
--- display/display.py
+++ display/display.py
@@ -6,12 +6,13 @@ import time
 from datetime import datetime
 import re
 from threading import Thread, Timer
-from socket import *
+import socket
 import logging
 logging.basicConfig()
-import couchdbkit
+import requests as req
 from apscheduler.scheduler import Scheduler
 import xml.etree.ElementTree as ET
+import requests
 
 from JetFileII import Font
 from JetFileII import Animate
@@ -19,16 +20,16 @@ from JetFileII import Format
 from JetFileII import Date
 from JetFileII import Message
 
-class LedDisplay():
-	"""Class to manage a led sign"""
 
-	def __init__(self, db_host, db_name, display_addr, display_port, endpoint):
+class LedDisplay(Thread):
 
-		#super(LedDisplay, self).__init__()
-		#Thread.__init__(self)
+	def __init__(self, addr, port):
+
+		super(LedDisplay, self).__init__()
+		Thread.__init__(self)
 		self.displayMsg = Message.DisplayControlWithoutChecksum
-		self.addr = display_addr
-		self.port = display_port
+		self.addr = addr
+		self.port = port
 		#configure display
 		#currently static values for my display
 		self.groupAddr = 1
@@ -36,63 +37,105 @@ class LedDisplay():
 		self.widthPixels = 120
 		self.heightPixels = 7
 		self.msg = None
-		self.dbserver = couchdbkit.Server(db_host)
-		self.db = self.dbserver.get_or_create_db(db_name)
-		self.endpoint = endpoint
+				
 
-	def sendMessage(self, text):
+	def createMessage(self, text):
 	 	self.msg = self.displayMsg.Create(1,text=text)
+
+	def run(self):
+
 		#create connection with display
-		s = socket(AF_INET, SOCK_STREAM)
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.settimeout(10)
+		try:
+	 		s.connect((self.addr,self.port))
 
- 		s.connect((self.addr,self.port))
-		#send message and close connection
-		print "Sending messsage to display"
-		s.send(self.msg)
-		s.close()
+			#send message and close connection
+			print "Sending message to screen"
+			s.send(self.msg)
+			s.close()
+			print "Message sent to screen, now waiting for next db hit"
+
+		except socket.timeout:
+			print ' Timed out, error sending message to sign, trying later.'
 
 
-	def query(self):
-			
-		text = ''
-		print 'Hitting database...'
-		for doc in self.db.all_docs( limit=5, descending=False, include_docs=True):
-			author = doc['doc']['author']
-			body = doc['doc']['body']
-			print 'message: ', body, ' by ', author
-			linea = '{red}{7x6}{slowest}{moveleftin}{moveleftout}{left}{left}{pause}' + body + ' > (' + str(author) + ')                         '
-			text += linea
+def filterChars(words):
+	"""
+	This function replaces non ASCII chars with __ 
+	to avoid sending special characters to the sign
+	"""
 
+	ns = "".join((c if ord(c) < 128 else '_' for c in words))
+	return ns
+
+
+def readData(host, display_addr, display_port, limit, key):
+		# #Timer(10, readData(host, db_name, display_addr, display_port))
+		# #get the data
+		# ops = {'limit':limit, 'sort':{'date': -1}}
+		heads = {'content-type':'application/json'}
+		# url = host
+
+		#print "Response from server: " + res.text
 		
-		print "Message built"
-		self.sendMessage(text)
+		#create message
+		text = ''
+		#build the request
 
+		params = {'_apikey': key, 'sort': '{"date":-1}', 'limit': limit}
+		res = requests.get(host, headers=heads, params = params)
+	
+		tmp = res.json()
+		#print res.text
+		
+		if res.status_code != 200:
+			print "Error in response:" + res.text
 
+		else:
+			print "succesfully aquired data from db"
+			rows = tmp
+
+			for row in rows:
+				body = filterChars(row['body'].encode("utf-8"))
+				author = filterChars(row['author'].encode("utf-8"))
+				print "mensaje: " + body + " by: " + author
+				linea = '{red}{7x6}{slowest}{moveleftin}{moveleftout}{left}{left}{pause}' + body + ' > (' + str(author) + ')                        ' 
+				text += linea
+
+			print "Message built"
+			message = LedDisplay(display_addr, display_port)
+			message.createMessage(text)	
+			message.start()
 
 def main():
 
+	"""
+	Read configuration
+	"""
+
 	config_file = "config.xml"
+
 	xml = ET.parse(config_file)
-	HOST_NAME = xml.find('host_name').text
+	HOST = xml.find('host').text
 	DB_NAME = xml.find('db_name').text
+	ENDPOINT = xml.find('endpoint').text
 	DISPLAY_ADDR = xml.find("display_addr").text
 	DISPLAY_PORT = int(xml.find('display_port').text)
-	ENDPOINT = xml.find("endpoint").text
-	SECONDS = int(xml.find("seconds").text)
-	USERNAME = xml.find("username").text
-	PASSWORD = xml.find("password").text
+	SECONDS = int(xml.find('seconds').text)
+	LIMIT = int(xml.find('limit').text)
+	KEY = xml.find('key').text
 
-	HOST = 'http://'+ USERNAME+ ':'+ PASSWORD + '@'+ HOST_NAME
+	print "Starting permitidorayar ..."
 
-	print "starting ..."
-	
-	#print HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT
-	#readData(HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT)
-	display = LedDisplay(HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT, ENDPOINT)
+	#HOST = HOST_NAME + DB_NAME + ENDPOINT
+	print HOST, DB_NAME, DISPLAY_ADDR, DISPLAY_PORT
+
 	# Start the scheduler
 	sched = Scheduler()
 
-	sched.add_interval_job(display.query, seconds=SECONDS )
+	# Schedule job_function to be called every two hours
+	sched.add_interval_job(readData, seconds=SECONDS, args = [HOST, DISPLAY_ADDR, DISPLAY_PORT, LIMIT, KEY], start_date = datetime.now())
 	sched.start()
 
 	try: 
@@ -106,10 +149,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
-	#configure http client
-	#base_url = "https://decolector.iriscouch.com/messages/"
-	#url =  base_url + '_all_docs'
-	#text = '{red}{7x6}{randomin}{randomout}Hello{nl}World{nl}JetFile{nl}'
-
-
